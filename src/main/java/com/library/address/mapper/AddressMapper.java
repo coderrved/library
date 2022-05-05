c59679106b7ee4e222e8a5b23a175bbfbbb2c29e
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2022-05-05 18:51:01 +0300

    created address mapper and added new method to user mapper

---
+++ src/main/java/com/library/address/mapper/AddressMapper.java
@@ -0,0 +1,21 @@
+package com.library.address.mapper;
+
+import com.library.address.entity.Address;
+import com.library.address.model.AddressRequestDto;
+import com.library.address.model.AddressResponseDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE )
+public interface AddressMapper {
+
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    List<AddressResponseDto> convertFromAddressesToAddressResponseDtos(List<Address> address);
+
+    List<Address> convertFromAddressRequestDtosToAddresses(List<AddressRequestDto> addressRequestDto);
+
+}
--- src/main/java/com/library/user/mapper/UserMapper.java
+++ src/main/java/com/library/user/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.library.user.mapper;
 
+import com.library.address.model.AddressResponseDto;
 import com.library.user.entity.User;
 import com.library.user.model.UserRequestDto;
 import com.library.user.model.UserResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -18,9 +20,11 @@ public interface UserMapper {
 
     UserResponseDto convertToUserResponseDto(UserRequestDto userRequestDto);
 
+    @Mapping(ignore = true, target = "addresses")
     User convertToUser(UserRequestDto userRequestDto);
 
-    List<UserResponseDto> convertAllToUserResponseDto(List<User> userList);
+    @Mapping(target = "userAddresses", source = "list")
+    UserResponseDto convertFromUserToUserResponseDto(User user, List<AddressResponseDto> list);
 
     UserResponseDto convertToUserResponseDto(User user);
 }
