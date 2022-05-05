e8ded7bdb37f3ce985b586eb82376fa56c485399
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2022-05-05 19:28:55 +0300

    changed database from h2 to postgres

--- src/main/java/com/library/LibraryApplication.java
+++ src/main/java/com/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class LibraryApplication {
 
 	public static void main(String[] args) {
