15708ea8e5625b11e7c4e7603e518e4b00af5b66
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2022-05-05 19:23:14 +0300

    refactor user service

--- src/main/java/com/library/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/library/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.library.user.service.impl;
 
+import com.library.address.entity.Address;
+import com.library.address.exceptions.AddressNotFound;
+import com.library.address.mapper.AddressMapper;
+import com.library.address.model.AddressResponseDto;
 import com.library.address.repository.AddressRepository;
 import com.library.user.entity.User;
-import com.library.user.exception.UserNotFound;
+import com.library.user.exceptions.UserNotFound;
 import com.library.user.mapper.UserMapper;
 import com.library.user.model.UserRequestDto;
 import com.library.user.model.UserResponseDto;
@@ -14,24 +18,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
     private final AddressRepository addressRepository;
-    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
-    public UserResponseDto findUserById(Long id){
+    public UserResponseDto findUserById(Long id) {
 
         final User user = findUser(id);
 
         final UserResponseDto userResponseDto = UserMapper.INSTANCE.convertToUserResponseDto(user);
 
-        logger.info("User : {} " ,userResponseDto);
+        final List<Address> addressesByUserID = findAddressesByUserID(id);
+
+        final List<AddressResponseDto> addressesOfUser = AddressMapper.INSTANCE.convertFromAddressesToAddressResponseDtos(addressesByUserID);
+
+        userResponseDto.setUserAddresses(addressesOfUser);
+
+        logger.info("User : {} ", userResponseDto);
 
         return userResponseDto;
     }
@@ -39,40 +50,36 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserResponseDto> findAllUsers() {
 
-        final List<User> userList = userRepository.findAll();
+        final List<User> users = userRepository.findAll();
 
-        final List<UserResponseDto> userResponseDtoList = UserMapper.INSTANCE.convertAllToUserResponseDto(userList);
-
-        logger.info("All Users : {} " ,userResponseDtoList);
-
-        return userResponseDtoList;
+        return getAllUsers(users);
     }
 
     @Transactional
     @Override
     public void saveUser(UserRequestDto userRequestDto) {
 
-        final User user = UserMapper.INSTANCE.convertToUser(userRequestDto);
+        final User user = populateAddressesOfUserById(userRequestDto);
 
         userRepository.save(user);
 
-        logger.info("Created User : {} " ,user);
+        logger.info("Created User : {} ", user);
     }
 
     @Override
     public UserResponseDto updateUserById(Long id, UserRequestDto userRequestDto) {
 
-        final User convertedUser = UserMapper.INSTANCE.convertToUser(userRequestDto);
+        final User updatedUser = populateAddressesOfUserById(userRequestDto);
 
         final User user = findUser(id);
 
-        convertedUser.setId(user.getId());
+        updatedUser.setId(user.getId());
 
-        userRepository.save(convertedUser);
+        userRepository.save(updatedUser);
 
         final UserResponseDto userResponseDto = UserMapper.INSTANCE.convertToUserResponseDto(userRequestDto);
 
-        logger.info(" Updated User : {} " ,userResponseDto);
+        logger.info("Updated User : {} ", userResponseDto);
 
         return userResponseDto;
     }
@@ -84,7 +91,7 @@ public class UserServiceImpl implements UserService {
 
         userRepository.delete(user);
 
-        logger.info("Deleted User : {} " ,user);
+        logger.info("Deleted User : {} ", user);
 
     }
 
@@ -92,12 +99,56 @@ public class UserServiceImpl implements UserService {
     public void deleteAllUser() {
 
         userRepository.deleteAll();
+
+        logger.info("Deleted All User");
     }
 
-    private User findUser(Long id){
+    private List<UserResponseDto> getAllUsers(List<User> users){
+
+        final List<UserResponseDto> userResponseDtos = new ArrayList<>();
+
+        for (User user : users) {
+
+            final List<AddressResponseDto> addressResponseDtos = AddressMapper.INSTANCE.convertFromAddressesToAddressResponseDtos(user.getAddresses());
+
+            UserResponseDto userResponseDto = UserMapper.INSTANCE.convertFromUserToUserResponseDto(user, addressResponseDtos);
+
+            userResponseDtos.add(userResponseDto);
+        }
+
+        logger.info("All Users : {} ", userResponseDtos);
+
+        return userResponseDtos;
+    }
+
+    private User populateAddressesOfUserById(UserRequestDto userRequestDto){
+
+        final User user = UserMapper.INSTANCE.convertToUser(userRequestDto);
+
+        final List<Address> addresses = AddressMapper.INSTANCE.convertFromAddressRequestDtosToAddresses(userRequestDto.getUserAddresses());
+
+        addresses.forEach(address -> address.setUsers(user));
+
+        user.setAddresses(addresses);
+
+        return user;
+    }
+
+    private User findUser(Long id) {
 
         return userRepository.findById(id).orElseThrow(
-                ()-> new UserNotFound("User with id :"+ id +" could not be found."));
+                () -> new UserNotFound("User with id :" + id + " could not be found."));
+    }
+
+    private List<Address> findAddressesByUserID(Long id){
+
+        final List<Address> addressesByUserId = addressRepository.findAddressesByUserId(id);
+
+        if (addressesByUserId.isEmpty()){
+            throw new AddressNotFound("The address of the user with ID number " + id + " could not be found.");
+        }
+
+        return addressesByUserId;
     }
 
 }
